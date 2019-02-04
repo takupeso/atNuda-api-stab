ce00cb5bec91404ee6a0fee932ae85b00d32ba8f
tyanakaz <xe314m@gmail.com>
2019-02-04 10:57:32 +0900

    First commit

--- rails-compose.sh
+++ rails-compose.sh
@@ -2,25 +2,18 @@
 
 touch Gemfile.lock
 
+
 # Full stack
 # docker-compose run api rails new . --database=mysql
 
 # API
-# docker-compose run --rm api rails new . --database=postgresql --api 
+docker-compose run api rails new . --database=mysql --api 
 
-# sleep 5 
+sleep 5 
 
 docker-compose build --force-rm
 
 sleep 5
 
-docker-compose up -d
-
-docker-compose exec api bash -c 'column=$(grep -n "encoding:" config/database.yml | cut -f 1 -d ":") && column=$((column + 1)) && sed -ie "${column}i username: postgres" config/database.yml'
-docker-compose exec api bash -c 'sed -ie "s/username: postgres/  username: postgres/" config/database.yml'
-
-docker-compose exec api bash -c 'column=$(grep -n "encoding:" config/database.yml | cut -f 1 -d ":") && column=$((column + 2)) && sed -ie "${column}i host: db" config/database.yml'
-docker-compose exec api bash -c 'sed -ie "s/host: db/  host: db/" config/database.yml'
-
-docker-compose exec api rails db:create
-
+docker-compose up -d && docker-compose exec api rake db:create
+docker-compose up -d && docker-compose exec api rake db:migrate
